5484872315f110cd8a66be9178e3abb6050d3488
mjvdende <maartenvdende@gmail.com>
2015-09-04 16:00:05 +0200

    add python libs

--- provision.sh
+++ provision.sh
@@ -13,3 +13,18 @@ sudo apt-get install -y python-wxgtk2.8
 sudo pip install robotframework
 sudo pip install robotframework-ride==1.5a1
 sudo pip install robotframework-selenium2library
+sudo pip install -U robotframework-difflibrary
+sudo pip install WebTest
+sudo pip install livetest
+sudo pip install --upgrade robotframework-httplibrary
+sudo pip install requests
+sudo pip install -U robotframework-requests
+sudo pip install robotframework-selenium2library
+sudo pip install robotframework-sudslibrary
+sudo pip install paramiko
+sudo pip install robotframework-sshlibrary
+
+echo -e "export PATH=$PATH:/home/vagrant/rf_demo_code/Test_libraries/Java_libraries/Hello_world/java_keywords/bin" >> /home/vagrant/.bashrc
+echo -e "export PATH=$PATH:/home/vagrant/rf_demo_code/Test_libraries/Java_libraries/Java_stack/robot/execution/lib" >> /home/vagrant/.bashrc
+echo -e "export PATH=$PATH:/home/vagrant/rf_demo_code/Test_libraries/Swing_libraries/Test_libs" >> /home/vagrant/.bashrc
+echo -e "export PATH=$PATH:/home/vagrant/rf_demo_code/Test_libraries/_Spec_files >> /home/vagrant/.bashrc
