be17002b160a5c3f8a190f3eb74225b8ae75fe3b
Viktor Clerc <viktor@clerc.nl>
2015-09-12 13:31:48 +0200

    First stab on Galen specific script

---
+++ provision-galen-github.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+echo I am provisioning the Galen Framework
+
+# install Galen framework
+sudo npm install -g galenframework-cli
+
+# clone git repository
+git clone https://github.com/galenframework/galen.git /home/vagrant/repoBase/galen
