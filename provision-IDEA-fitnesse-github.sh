41d7a9656a53966a4bdf49c413820751674d6b36
Viktor Clerc <viktor@clerc.nl>
2015-09-15 10:19:57 +0200

    Added gradle import stuff so more is downloaded on beforehand

--- provision-IDEA-fitnesse-github.sh
+++ provision-IDEA-fitnesse-github.sh
@@ -8,6 +8,13 @@ wget -O /tmp/intellij.tar.gz http://download.jetbrains.com/idea/ideaIC-14.1.4.ta
 tar -zxf /tmp/intellij.tar.gz -C /home/vagrant &&
 cd /home/vagrant/idea-IC-141.1532.4/bin
 
+mkdir -p /home/vagrant/.IdeaIC14/config/plugins
+cd /home/vagrant/.IdeaIC14/config/plugins
+wget https://github.com/gshakhn/idea-fitnesse/releases/download/1.0.4/idea-fitnesse-1.0.4.zip
+unzip idea-fitnesse-1.0.4.zip
+
+cd /home/vagrant/.IdeaIC14/config/plugins
+
 # create some kind of dekstop entry
 
 echo "[Desktop Entry]
@@ -27,6 +34,10 @@ sudo desktop-file-install /home/vagrant/idea.desktop
 cd /home/vagrant/repoBase
 git clone https://github.com/amolenaar/beefing-up-fitnesse.git /home/vagrant/repoBase/beefing-up-fitnesse
 sudo chown -R vagrant:vagrant /home/vagrant/repoBase/beefing-up-fitnesse
+
+# set up IntelliJ project files
+cd /home/vagrant/repoBase/beefing-up-fitnesse
+./gradlew idea
 
 # starting the IDE yields a GUI error
 # ./idea.sh
